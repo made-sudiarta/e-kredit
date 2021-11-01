958fe1d4693352fdcaac07a7ffb6e4bf20d63304
DESKTOP-01IGM35\sudia <made.sudiarta220595@gmail.com>
2021-11-02 01:57:48 +0800

    Fixing Login and Registration page

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -12,26 +12,28 @@ class CustomAuthController extends Controller
 {
     public function index()
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return redirect('dashboard');
         }
         return view('auth.login');
-    }  
-      
+    }
+
 
     public function customLogin(Request $request)
     {
         $request->validate([
+            'access' => 'required',
             'email' => 'required',
             'password' => 'required',
         ]);
-   
+
         $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
+        //if (Auth::attempt($credentials)) {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'access' => $request->access])) {
             return redirect()->intended('dashboard')
-                        ->withSuccess('Signed in');
+                ->withSuccess('Signed in');
         }
-  
+
         return redirect("login")->withSuccess('Login details are not valid');
     }
 
@@ -39,52 +41,60 @@ class CustomAuthController extends Controller
 
     public function registration()
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return redirect('dashboard');
         }
         return view('auth.registration');
     }
-      
+
 
     public function customRegistration(Request $request)
-    {  
+    {
         $request->validate([
+            'access' => 'required',
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
         ]);
-           
+
         $data = $request->all();
         $check = $this->create($data);
-         
+
         return redirect("dashboard")->withSuccess('You have signed-in');
     }
 
 
     public function create(array $data)
     {
-      return User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => Hash::make($data['password'])
-      ]);
-    }    
-    
+        return User::create([
+            'access' => $data['access'],
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
+
 
     public function dashboard()
     {
-        if(Auth::check()){
-            return view('dashboard');
+        if (Auth::check()) {
+            $access = Auth::user()->access;
+            if ($access == "member") {
+                return view('auth.member');
+            } else {
+                return view('auth.employee');
+            }
         }
-  
+
         return redirect("login")->withSuccess('You are not allowed to access');
     }
-    
 
-    public function signOut() {
+
+    public function signOut()
+    {
         Session::flush();
         Auth::logout();
-  
+
         return Redirect('login');
     }
 }
--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -11,25 +11,27 @@
 
                         <form action="{{ route('register.custom') }}" method="POST">
                             @csrf
+                            <select class="form-select mb-3" name="access" id="access" aria-label="Default select example">
+                                <option hidden selected>Account Access</option>
+                                <option value="member">Member</option>
+                                <option value="employee">Employee</option>
+                            </select>
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Name" id="name" class="form-control" name="name"
-                                    required autofocus>
+                                <input type="text" placeholder="Name" id="name" class="form-control" name="name" required autofocus>
                                 @if ($errors->has('name'))
                                 <span class="text-danger">{{ $errors->first('name') }}</span>
                                 @endif
                             </div>
 
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email_address" class="form-control"
-                                    name="email" required autofocus>
+                                <input type="text" placeholder="Email" id="email_address" class="form-control" name="email" required autofocus>
                                 @if ($errors->has('email'))
                                 <span class="text-danger">{{ $errors->first('email') }}</span>
                                 @endif
                             </div>
 
                             <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="form-control"
-                                    name="password" required>
+                                <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
                                 @if ($errors->has('password'))
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
                                 @endif
